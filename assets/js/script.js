b33bb2faa325fc575e71749488d7f92c6359a7b4
Everen-John <everen.john@gmail.com>
2021-07-28 16:38:35 +0800

    Added LocalStorage and Loadbooks initially when page loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,63 +6,67 @@ function Book(name, author, type) {
 }
 
 // typrwiter text
-var TxtType = function(el, toRotate, period) {
+var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
+  this.txt = "";
   this.tick();
   this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
+TxtType.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
-  this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
   var that = this;
   var delta = 200 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period;
-  this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
+  if (this.isDeleting) {
+    delta /= 2;
   }
 
-  setTimeout(function() {
-  that.tick();
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
   }, delta);
 };
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
   }
+
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
   document.body.appendChild(css);
 };
-
+//Load all books from LocalStorage, put outside because the window.onload function is broken
+LoadBooks();
 
 // Display Constructor
 function Display() {}
@@ -116,12 +120,12 @@ Display.prototype.show = function (type, displayMessage) {
 let libraryForm = document.getElementById("libraryForm");
 libraryForm.addEventListener("submit", libraryFormSubmit);
 
+//Submit book
 function libraryFormSubmit(e) {
   console.log("YOu have submitted library form");
   let name = document.getElementById("bookName").value;
   let author = document.getElementById("author").value;
   let type = document.getElementById("type").value;
- 
 
   let book = new Book(name, author, type);
   console.log(book);
@@ -132,12 +136,52 @@ function libraryFormSubmit(e) {
     display.add(book);
     display.clear();
     display.show("success", "Your book has been successfully added");
+    addBooktoLocalStorage(book);
   } else {
     // Show error to the user
     display.show("danger", "Sorry you cannot add this book");
   }
 
   e.preventDefault();
+}
+
+//Store all the data to the localStorage
+function addBooktoLocalStorage(book) {
+  let addedBook = book;
+  let allbooks;
+
+  let booksAddition = new Promise((resolve, reject) => {
+    allbooks = JSON.parse(window.localStorage.getItem("books"));
+    console.log("allbooks right now:" + JSON.stringify(allbooks));
+    allbooks = allbooks ? [...allbooks, addedBook] : [addedBook];
+    window.localStorage.setItem("books", JSON.stringify(allbooks));
+    resolve(allbooks);
+  });
+
+  booksAddition.then((allbooks) => {
+    console.log(JSON.stringify(window.localStorage.getItem("books")));
+  });
+}
+
+//Load Books Table onLoad
+function LoadBooks() {
+  let allbooks = JSON.parse(window.localStorage.getItem("books"));
+  tableBody = document.getElementById("tableBody");
+  let countBooks = 0;
+  let uiString;
+  allbooks.map((book, allbooks) => {
+    countBooks++;
+    uiString += `<tr id="${countBooks}">
+    <td>${book.name}</td>
+    <td>${book.author}</td>
+    <td>${book.type}</td>
+    <td><button class="btn btn-success" id="edit" onclick="editfunction('${book.name}','${book.author}','${book.type}','${countBooks}')">Edit</button>
+    </td?
+    <td><button class="btn btn-danger" id="edit" onclick="deletefunction('${countBooks}')">Delete</button>
+    </td>
+</tr>`;
+  });
+  tableBody.innerHTML += uiString;
 }
 
 //edit function
@@ -153,11 +197,12 @@ function deletefunction(pbookid) {
   document.getElementById(pbookid).style.display = "none";
 }
 
-window.onload=function(){
-  document.getElementById('loading').style.display="none";
-  document.getElementById('body').style.display="block";
-}
+window.onload = function () {
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("body").style.display = "block";
+};
 // Todos"
-// 1. Store all the data to the localStorage
+// 1.
+
 // 2. Give another column as an option to delete the book
 // 3. Add a scroll bar to the view.
